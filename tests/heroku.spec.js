18b73f0c5a34d6f824888755e39cfb1793786991
IfeanyiOkpana <benysure@gmail.com>
2026-09-06 15:36:42 +0100

    Added Assertions

--- tests/heroku.spec.js
+++ tests/heroku.spec.js
@@ -3,12 +3,16 @@ import { test, expect } from '@playwright/test';
 test.describe('Test', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/');
+    await expect(page).toHaveTitle(/The Internet/);
   });
 
 test('Add/Remove Elements', async ({ page }) => {
   await page.getByRole('link', { name: "Add/Remove Elements"}).click();
   await page.getByRole('button', { name: "Add Element"}).click();
   await page.getByRole('button', { name: "Delete"}).click();
+
+  await expect(page.getByRole('heading', { name: "Add/Remove Elements"})).toBeVisible();
+  await expect(page).toHaveURL('https://the-internet.herokuapp.com/add_remove_elements/');
 });
 
 test('Checkboxes', async ({ page }) => {
