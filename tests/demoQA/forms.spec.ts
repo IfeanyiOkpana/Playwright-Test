cf8c0ba27d53cc8555baf7f50436908246a4c69e
IfeanyiOkpana <benysure@gmail.com>
2026-09-08 17:18:56 +0100

    Tested a portion of form functionality

---
+++ tests/demoQA/forms.spec.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Forms', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://demoqa.com/');
+    });
+
+    test('Test Form Functionality', async ({ page }) => {
+        await page.getByRole('heading', { name: "Forms"}).click();
+        await page.getByText("Forms", { exact: true}).click();
+
+
+        await page.getByText("Practice Form", { exact: true}).click();
+
+        await page.locator('#firstName').fill("Mike");
+        await page.locator('#lastName').fill("Stone");
+
+        await expect(page.getByRole('heading', { name: "Practice Form"})).toBeVisible();
+    });
+});
