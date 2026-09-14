676c0d5f30caafaf460ae294262dd6177682d9cb
IfeanyiOkpana <benysure@gmail.com>
2026-09-14 21:51:55 +0100

    Tested "Make Deposits" link

--- tests/parabank/about-us.spec.ts
+++ tests/parabank/about-us.spec.ts
@@ -10,7 +10,8 @@ test.describe('Parabank', () => {
 
         await expect(page.getByRole('heading', { name: "ParaSoft Demo Website"})).toBeVisible();
     });
-    test('Test Make Deposits Link', async ({ page }) => {
+
+    test('Make Deposits Link', async ({ page }) => {
         await page.getByRole('link', { name: "Make Deposits"}).click();
-    })
+    });
 });
